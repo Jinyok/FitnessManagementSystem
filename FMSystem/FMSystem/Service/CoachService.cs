9e6cae12946499a4d02b79d4f56204b39d296b49
AlwayConfident <62639467+AlwayConfident@users.noreply.github.com>
2020-06-17 04:23:30 +0800

    Update CoachService.cs

--- FMSystem/FMSystem/Service/CoachService.cs
+++ FMSystem/FMSystem/Service/CoachService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,21 +13,21 @@ namespace FMSystem.Service
 {
     public class CoachService
     {
-        private fmsContext context;
-        public void Initialize(IServiceProvider serviceProvider)
+        private static fmsContext context;
+        public static void Initialize(IServiceProvider serviceProvider)
         {
             context = new fmsContext(
                 serviceProvider.GetRequiredService<
                     DbContextOptions<fmsContext>>());
         }
-        public Coach GetCoachesById(int id)
+        public static Coach GetCoachesById(int id)
         {
             Coach coaches = context.Coach.Single(c => c.CoachId == id);
 
             return coaches;
         }
 
-        public List<Coach> GetCoachesByName(string name)
+        public static List<Coach> GetCoachesByName(string name)
         {
             List<Coach> coaches = context.Coach.Where(c => c.Name == name).ToList();
 
@@ -35,7 +35,7 @@ namespace FMSystem.Service
 
         }
 
-        public void AddCoach(Coach coach)
+        public static void AddCoach(Coach coach)
         {
             if (coach.CoachId > 0)
             {
@@ -45,13 +45,13 @@ namespace FMSystem.Service
             return;
         }
 
-        public void DeleteCoach(int id)
+        public static void DeleteCoach(int id)
         {
             context.Coach.Remove(GetCoachesById(id));
             context.SaveChanges();
         }
 
-        public void UpdateCoach(Coach coach)
+        public static void UpdateCoach(Coach coach)
         {
             Coach temp = context.Coach.Find(coach.CoachId);
             if (temp == null)
