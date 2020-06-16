cedbcf0a9cc9bf2ceadc02a85e57cb2f02791424
Jinyok <1506493779@qq.com>
2020-06-16 23:33:01 +0800

    feat: add text controller

---
+++ FMSystem/FMSystem/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FMSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FMSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly fmsContext _context;
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(fmsContext context, ILogger<TestController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            //call fun;
+            var obj = 3;
+            return Ok(obj);
+        }
+    }
+}
