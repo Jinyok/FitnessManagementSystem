bde5fb5891a0b70e5b89a57e5130910c33ec1855
Jinyok <1506493779@qq.com>
2020-06-17 17:11:53 +0800

    add ResponseModel

---
+++ FMSystem/FMSystem/Models/ResponseModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMSystem.Models
+{
+    public class ResponseModel
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+        public void SetSuccess(string message = "成功")
+        {
+            Code = 200;
+            Message = message;
+        }
+        public void SetFailed(string message = "失败")
+        {
+            Message = message;
+            Code = 999;
+        }
+
+        public void SetData(object data)
+        {
+            Data = data;
+        }
+    }
+}
