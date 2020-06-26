93c004e186773dee4bd92ea7df72cb6c89ba2060
AlwayConfident <62639467+AlwayConfident@users.noreply.github.com>
2020-06-26 17:41:49 +0800

    修改输入的PhoneNo类型

--- FMSystem/FMSystem/Controllers/CoachesController.cs
+++ FMSystem/FMSystem/Controllers/CoachesController.cs
@@ -33,7 +33,7 @@ namespace FMSystem.Controllers
         public IActionResult GetCoachesByName(string name) => Ok(_coachService.GetCoachesByName(name));
 
         [HttpPost]
-        public IActionResult AddCoach(string name, long phoneno, string email) => Ok(_coachService.AddCoach(name, phoneno, email));
+        public IActionResult AddCoach(string name, string phoneno, string email) => Ok(_coachService.AddCoach(name, phoneno, email));
 
         [HttpDelete]
         public IActionResult DeleteCoach(int id) => Ok(_coachService.DeleteCoach(id));
