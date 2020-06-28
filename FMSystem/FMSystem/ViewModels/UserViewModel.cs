be5909d94b3b86d4d25f34aa229f5e31e8d2e6f0
硫硝酸酸雨 <60508733+sanseiuP@users.noreply.github.com>
2020-06-28 12:05:04 +0800

    Update UserViewModel.cs

--- FMSystem/FMSystem/ViewModels/UserViewModel.cs
+++ FMSystem/FMSystem/ViewModels/UserViewModel.cs
@@ -9,7 +9,9 @@ namespace FMSystem.ViewModels
     {
         public long UserId { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
         public string Role { get; set; }
         public int Number { get; set; }
+        public long CreateTime { get; set; }
     }
 }
